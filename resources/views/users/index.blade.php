0d10eb0f5283c36b70e5369dccfa266cdd02bf33
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-03-22 21:30:41 -0600

    Logica de controladores y vistas con datos de prueba

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 mx-auto">
+            
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Email</th>
+                    <th>&nbsp;</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach($users as $user)
+                  <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                      <form action="{{ route('users.destroy', $user) }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <input 
+                          type="submit" 
+                          value="Eliminar" 
+                          class="btn btn-sm btn-danger"
+                          onclick="return confirm('Desea elminar... ?')">
+                      </form>
+                    </td>
+                  </tr>
+                  @endforeach()
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+    </body>
+</html>
